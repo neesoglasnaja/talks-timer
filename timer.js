c96666ebc9c2192924e9e5492f71c1c1b7912eca
Aleksandra Shinkevich <neesa.web@gmail.com>
2017-09-17 22:31:15 +0300

    Add control buttons

--- timer.js
+++ timer.js
@@ -17,6 +17,7 @@
 
 	var Timer = function(skipUpdate) {
 		var countFrom = 0;
+		var left = 0;
 		var timerNode = document.querySelector('.timer');
 		var interval;
 
@@ -31,6 +32,25 @@
 						updateScreen(timerDate)
 					}, 100)
 				}
+			},
+			pause: function() {
+				clearInterval(interval);
+			},
+			clear: function() {
+				clearInterval(interval);
+				countFrom = 0;
+				left = 0;
+			},
+			continue: function() {
+				var now = new Date()
+				var timerDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + left.getMinutes(), now.getSeconds() + left.getSeconds(), now.getMilliseconds());
+				updateScreen(timerDate);
+				interval = setInterval(function(){
+						updateScreen(timerDate)
+					}, 100)
+			},
+			addTime: function(minutes) {
+
 			}
 		};
 
@@ -38,9 +58,10 @@
 			var rightNow = new Date();
 			var isPositive = timerDate > rightNow;
 			var diff = isPositive ?  ( new Date(timerDate - rightNow)): ( new Date(rightNow - timerDate));
+			left = diff;
 			timerNode.textContent = (isPositive ? '+': '-') + diff.getMinutes() + ':' + diff.getSeconds();
 			for (var i = 0; i < config.chunks.length; i++) {
-				if(config.chunks[i].minutes> diff.getMinutes()){
+				if (config.chunks[i].minutes> diff.getMinutes()) {
 					document.bgColor = config.chunks[i].color
 				}
 			}
@@ -48,7 +69,25 @@
 	}	
 
 
-	var timer = new Timer(true);
+	var timer = new Timer(false);
 	timer.init(20, config);
+
+	var pauseNode = document.querySelector('.pause');
+	var continueNode = document.querySelector('.continue');
+	var clearNode = document.querySelector('.clear');
+	var addNode = document.querySelector('.add');
+
+	pauseNode.addEventListener('click', function() {
+		timer.pause();
+	});
+	continueNode.addEventListener('click', function() {
+		timer.continue();
+	});
+	clearNode.addEventListener('click', function() {
+		timer.clear();
+	});
+	addNode.addEventListener('click', function() {
+		timer.addTime(5);
+	});
 
 })();
